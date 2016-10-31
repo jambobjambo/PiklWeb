6f4c5bf5b3c6fbf24adaad1b111510d4ac6bb3b2
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-31 10:47:52 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -1,16 +1,12 @@
-#import tensorflow as tf
-#from flask import Flask
-#app = Flask(__name__)
-#import urllib
-import webapp2
+import tensorflow as tf
+from flask import Flask
+app = Flask(__name__)
+import urllib
 
-#@app.route('/')
 
-class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello World!')
-		'''urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
+@app.route('/')
+def home():
+		urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
 		# Read in the image_data
 		image_data = tf.gfile.FastGFile('image.jpg', 'rb').read()
 
@@ -34,10 +30,7 @@ class MainPage(webapp2.RequestHandler):
 		for node_id in top_k:
 			human_string = label_lines[node_id]
 			score = predictions[0][node_id]
-			self.response.write("{ '%s' : { 'score' : '%.5f' } }" % (human_string, score))'''
+			return("{ '%s' : { 'score' : '%.5f' } }" % (human_string, score))
 
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=8080)
-
-app = webapp2.WSGIApplication([
-	('/', MainPage),], debug= True)
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080)
