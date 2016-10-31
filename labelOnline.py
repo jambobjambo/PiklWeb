97fb8bef5f95b7b82dc06f534862c32a93ba115f
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-31 09:01:59 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 from flask import Flask
 app = Flask(__name__)
-
+import urllib
 
 @app.route('/')
 def hello():
+	urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
 	# Read in the image_data
-	image_data = tf.gfile.FastGFile('testdata/download.jpg', 'rb').read()
+	image_data = tf.gfile.FastGFile('image.jpg', 'rb').read()
 
 	# Loads label file, strips off carriage return
 	label_lines = [line.rstrip() for line 
