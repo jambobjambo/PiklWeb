564ac6f0be3a0df659d7c4764cd0ba27f59d5f36
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-31 20:58:04 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -1,5 +1,6 @@
 import logging
-
+import urllib
+import tensorflow as tf
 from flask import Flask
 
 
@@ -8,8 +9,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
+	# Read in the image_data
+	image_data = tf.gfile.FastGFile('image.jpg', 'rb').read()
+
+	# Loads label file, strips off carriage return
+	label_lines = [line.rstrip() for line 
+                   in tf.gfile.GFile("retrained_labels.txt")]
+
+	# Unpersists graph from file
+	with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+   		graph_def = tf.GraphDef()
+   		graph_def.ParseFromString(f.read())
+   		_ = tf.import_graph_def(graph_def, name='')
+
+	with tf.Session() as sess:
+		softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+		predictions = sess.run(softmax_tensor, \
+             {'DecodeJpeg/contents:0': image_data})
+
+	top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+	for node_id in top_k:
+		human_string = label_lines[node_id]
+		score = predictions[0][node_id]
+		return("{ '%s' : { 'score' : '%.5f' } }" % (human_string, score))
 
 
 @app.errorhandler(500)
