3f64e9da4e97360d9c7a204808392bd1b0c4d2fb
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-31 20:29:28 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -4,7 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return('This better work')	
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080)
+	return('This better work')
