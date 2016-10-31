34cf34b52d66dcc41195e2b0dcc9a352afc6c461
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-31 13:09:36 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -1,12 +1,18 @@
-import tensorflow as tf
+#import tensorflow as tf
 from flask import Flask
 app = Flask(__name__)
-import urllib
+#import urllib
 
 
 @app.route('/')
 def home():
-	urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
+	return('This better work')	
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080)
+
+'''
+urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
 	# Read in the image_data
 	image_data = tf.gfile.FastGFile('image.jpg', 'rb').read()
 
@@ -31,6 +37,4 @@ def home():
 		human_string = label_lines[node_id]
 		score = predictions[0][node_id]
 		return("{ '%s' : { 'score' : '%.5f' } }" % (human_string, score))
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080)
+'''
