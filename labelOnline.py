56e5605ec75547ff0f2314dc220d164bc8527ce5
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-31 11:21:50 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return 'Hello World'
-		'''urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
+		
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080)
+
+'''
+urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
 		# Read in the image_data
 		image_data = tf.gfile.FastGFile('image.jpg', 'rb').read()
 
@@ -31,7 +37,5 @@ def home():
 		for node_id in top_k:
 			human_string = label_lines[node_id]
 			score = predictions[0][node_id]
-			return("{ '%s' : { 'score' : '%.5f' } }" % (human_string, score))'''
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080)
+			return("{ '%s' : { 'score' : '%.5f' } }" % (human_string, score))
+'''
