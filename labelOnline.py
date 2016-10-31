c4ac03ed60a5c62e0f4f6b8de97f5665bf20391e
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-31 20:35:31 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -1,7 +1,13 @@
 
+import logging
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return('This better work')
+	return 'This better work'
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
