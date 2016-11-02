94d250bc55b15d9e2ec8a5276bb0fee80ce5ddd0
jambobjambo <jamie.a.mcinally@gmail.com>
2016-11-02 12:47:36 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -14,4 +14,4 @@ def hello():
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
