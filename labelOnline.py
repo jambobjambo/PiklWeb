28a68decfd77d339d284fdb051b182ebfeb0f4f8
jambobjambo <jamie.a.mcinally@gmail.com>
2016-11-03 10:09:08 +0000

    Add files via upload

--- labelOnline.py
+++ labelOnline.py
@@ -1,4 +1,3 @@
-import urllib
 import tensorflow as tf
 from flask import Flask
 
@@ -8,11 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/piklappdev.appspot.com/o/imagestmp%2Fimage3.jpg?alt=media&token=4a5c08b4-8d48-45f8-b9ab-5b49dbb14e51", "image.jpg")
 	image_data = tf.gfile.FastGFile('image.jpg', 'rb').read()
 	label_lines = [line.rstrip() for line in tf.gfile.GFile("retrained_labels.txt")]
 	with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
-   		return "Hello World"
+   		graph_def = tf.GraphDef()
+   		graph_def.ParseFromString(f.read())
+   		_ = tf.import_graph_def(graph_def, name='')
+
+		with tf.Session() as sess:
+			softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+			predictions = sess.run(softmax_tensor, \
+   				{'DecodeJpeg/contents:0': image_data})
+
+   		top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+   		for node_id in top_k:
+			human_string = label_lines[node_id]
+			score = predictions[0][node_id]
+			return("{ '%s' : { 'score' : '%.5f' } }" % (human_string, score))
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
